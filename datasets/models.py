c5e2f007df96a4fd44321dc1fcc66f4f79689f58
ivillaabrille <isayavillaabrille@gmail.com>
2018-01-05 19:22:29 +0800

    added functions for superusers to approve datasets

--- datasets/models.py
+++ datasets/models.py
@@ -3,16 +3,12 @@ from datetime import datetime
 from django.contrib.auth.models import User
 
 class DataSet(models.Model):
-    STATUS_CHOICES = (
-        ('Approved', 'Approved'),
-        ('Not yet Approved', 'Not yet Approved'),
-    )
 
     DataSet_Title = models.CharField(max_length=100)
     DataSet_Description = models.TextField()
     DataSet_Poster = models.ForeignKey(User)
     DataSet_Posted = models.DateTimeField(default=datetime.now)
-    DataSet_Status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='Not yet Approved')
+    DataSet_Status = models.BooleanField(default=False)
     Data = models.TextField(blank=True)
     jsonifiedData_url = models.CharField(max_length=100, blank=True)
     csvfiedData_url = models.CharField(max_length=100, blank=True)
