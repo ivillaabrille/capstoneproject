2661c994835a4f354a22355676bbce8851190ace
ivillaabrille <isayavillaabrille@gmail.com>
2018-01-10 07:08:14 +0800

    new frontend integrated

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -127,7 +127,7 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "assets"),
@@ -154,7 +154,7 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'assets'),
     )
